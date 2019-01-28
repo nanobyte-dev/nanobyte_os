ba9def45e5a59ecdec0570a3417920cacc532b9a
Tiberiu Chibici <chibicitiberiu@gmail.com>
2019-01-28 11:10:42 +0200

    Fixed tabs/spaces

--- src/main.asm
+++ src/main.asm
@@ -6,7 +6,7 @@ bits 16
 
 
 start:
-	jmp main
+    jmp main
 
 
 ;
@@ -16,46 +16,46 @@ start:
 ;
 puts:
     ; save registers we will modify
-	push si
-	push ax
+    push si
+    push ax
     push bx
 
 .loop:
-	lodsb			    ; loads next character in al
-	or al, al		    ; verify if next character is null?
-	jz .done
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done
 
-	mov ah, 0x0E	    ; call bios interrupt
+    mov ah, 0x0E        ; call bios interrupt
     mov bh, 0           ; set page number to 0
-	int 0x10
+    int 0x10
 
-	jmp .loop
+    jmp .loop
 
 .done:
     pop bx
-	pop ax
-	pop si	
-	ret
-	
+    pop ax
+    pop si    
+    ret
+    
 
 main:
     ; setup data segments
-	mov ax, 0           ; can't set ds/es directly
-	mov ds, ax
-	mov es, ax
+    mov ax, 0           ; can't set ds/es directly
+    mov ds, ax
+    mov es, ax
     
-	; setup stack
-	mov ss, ax
-	mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
 
-	; print hello world message
-	mov si, msg_hello
-	call puts
+    ; print hello world message
+    mov si, msg_hello
+    call puts
 
-	hlt
+    hlt
 
 .halt
-	jmp .halt
+    jmp .halt
 
 
 
