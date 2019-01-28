304003f26dfe004497dcb2bb6bfa3d12784994d3
Tiberiu Chibici <chibicitiberiu@gmail.com>
2019-01-28 11:02:50 +0200

    Printed 'hello world' message to screen.

--- src/main.asm
+++ src/main.asm
@@ -2,11 +2,64 @@ org 0x7C00
 bits 16
 
 
+%define ENDL 0x0D, 0x0A
+
+
+start:
+	jmp main
+
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
+puts:
+    ; save registers we will modify
+	push si
+	push ax
+    push bx
+
+.loop:
+	lodsb			    ; loads next character in al
+	or al, al		    ; verify if next character is null?
+	jz .done
+
+	mov ah, 0x0E	    ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+	int 0x10
+
+	jmp .loop
+
+.done:
+    pop bx
+	pop ax
+	pop si	
+	ret
+	
+
 main:
-	hlt
+    ; setup data segments
+	mov ax, 0           ; can't set ds/es directly
+	mov ds, ax
+	mov es, ax
     
-.halt:
+	; setup stack
+	mov ss, ax
+	mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
+
+	; print hello world message
+	mov si, msg_hello
+	call puts
+
+	hlt
+
+.halt
 	jmp .halt
+
+
+
+msg_hello: db 'Hello world!', ENDL, 0
 
 
 times 510-($-$$) db 0
