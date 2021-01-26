7b9a3d1089448f25856a20a8f9a0cdad578e8c80
Tiberiu Chibici <chibicitiberiu@gmail.com>
2021-01-27 00:00:27 +0200

    Split into bootloader and kernel, made floppy image use FAT12, and added FAT12 headers to bootloader

--- Makefile
+++ Makefile
@@ -3,9 +3,43 @@ ASM=nasm
 SRC_DIR=src
 BUILD_DIR=build
 
-$(BUILD_DIR)/main_floppy.img: $(BUILD_DIR)/main.bin
-    cp $(BUILD_DIR)/main.bin $(BUILD_DIR)/main_floppy.img
-    truncate -s 1440k $(BUILD_DIR)/main_floppy.img
-    
-$(BUILD_DIR)/main.bin: $(SRC_DIR)/main.asm
-    $(ASM) $(SRC_DIR)/main.asm -f bin -o $(BUILD_DIR)/main.bin
+.PHONY: all floppy_image kernel bootloader clean always
+
+#
+# Floppy image
+#
+floppy_image: $(BUILD_DIR)/main_floppy.img
+
+$(BUILD_DIR)/main_floppy.img: bootloader kernel
+	dd if=/dev/zero of=$(BUILD_DIR)/main_floppy.img bs=512 count=2880
+	mkfs.fat -F 12 -n "NBOS" $(BUILD_DIR)/main_floppy.img
+	dd if=$(BUILD_DIR)/bootloader.bin of=$(BUILD_DIR)/main_floppy.img conv=notrunc
+	mcopy -i $(BUILD_DIR)/main_floppy.img $(BUILD_DIR)/kernel.bin "::kernel.bin"
+
+#
+# Bootloader
+#
+bootloader: $(BUILD_DIR)/bootloader.bin
+
+$(BUILD_DIR)/bootloader.bin: always
+	$(ASM) $(SRC_DIR)/bootloader/boot.asm -f bin -o $(BUILD_DIR)/bootloader.bin
+
+#
+# Kernel
+#
+kernel: $(BUILD_DIR)/kernel.bin
+
+$(BUILD_DIR)/kernel.bin: always
+	$(ASM) $(SRC_DIR)/kernel/main.asm -f bin -o $(BUILD_DIR)/kernel.bin
+
+#
+# Always
+#
+always:
+	mkdir -p $(BUILD_DIR)
+
+#
+# Clean
+#
+clean:
+	rm -rf $(BUILD_DIR)/*
---
+++ src/bootloader/boot.asm
@@ -0,0 +1,98 @@
+org 0x7C00
+bits 16
+
+
+%define ENDL 0x0D, 0x0A
+
+
+;
+; FAT12 header
+; 
+jmp short start
+nop
+
+bdb_oem:                    db 'MSWIN4.1'           ; 8 bytes
+bdb_bytes_per_sector:       dw 512
+bdb_sectors_per_cluster:    db 1
+bdb_reserved_sectors:       dw 1
+bdb_fat_count:              db 2
+bdb_dir_entries_count:      dw 0E0h
+bdb_total_sectors:          dw 2880                 ; 2880 * 512 = 1.44MB
+bdb_media_descriptor_type:  db 0F0h                 ; F0 = 3.5" floppy disk
+bdb_sectors_per_fat:        dw 9                    ; 9 sectors/fat
+bdb_sectors_per_track:      dw 18
+bdb_heads:                  dw 2
+bdb_hidden_sectors:         dd 0
+bdb_large_sector_count:     dd 0
+
+; extended boot record
+ebr_drive_number:           db 0                    ; 0x00 floppy, 0x80 hdd, useless
+                            db 0                    ; reserved
+ebr_signature:              db 29h
+ebr_volume_id:              db 12h, 34h, 56h, 78h   ; serial number, value doesn't matter
+ebr_volume_label:           db 'NANOBYTE OS'        ; 11 bytes, padded with spaces
+ebr_system_id:              db 'FAT12   '           ; 8 bytes
+
+;
+; Code goes here
+;
+
+start:
+    jmp main
+
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done
+
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si    
+    ret
+    
+
+main:
+    ; setup data segments
+    mov ax, 0           ; can't set ds/es directly
+    mov ds, ax
+    mov es, ax
+    
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
+
+    ; print hello world message
+    mov si, msg_hello
+    call puts
+
+    hlt
+
+.halt
+    jmp .halt
+
+
+
+msg_hello: db 'Hello world!', ENDL, 0
+
+
+times 510-($-$$) db 0
+dw 0AA55h
