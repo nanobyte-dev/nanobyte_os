582793ff722d38acbf8c2cfcb31b807039591cf3
Tiberiu Chibici <chibicitiberiu@gmail.com>
2021-07-10 15:41:55 +0300

    Implemented disk read methods in stage2

---
+++ src/bootloader/stage2/disk.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "stdint.h"
+
+typedef struct {
+    uint8_t id;
+    uint16_t cylinders;
+    uint16_t sectors;
+    uint16_t heads;
+} DISK;
+
+bool DISK_Initialize(DISK* disk, uint8_t driveNumber);
+bool DISK_ReadSectors(DISK* disk, uint32_t lba, uint8_t sectors, uint8_t far* dataOut);
--- src/bootloader/stage2/x86.asm
+++ src/bootloader/stage2/x86.asm
@@ -2,6 +2,39 @@ bits 16
 
 section _TEXT class=CODE
 
+
+;
+; U4D
+;
+; Operation:      Unsigned 4 byte divide
+; Inputs:         DX;AX   Dividend
+;                 CX;BX   Divisor
+; Outputs:        DX;AX   Quotient
+;                 CX;BX   Remainder
+; Volatile:       none
+;
+global __U4D
+__U4D:
+    shl edx, 16         ; dx to upper half of edx
+    mov dx, ax          ; edx - dividend
+    mov eax, edx        ; eax - dividend
+    xor edx, edx
+
+    shl ecx, 16         ; cx to upper half of ecx
+    mov cx, bx          ; ecx - divisor
+
+    div ecx             ; eax - quot, edx - remainder
+    mov ebx, edx
+    mov ecx, edx
+    shr ecx, 16
+
+    mov edx, eax
+    shr edx, 16
+    
+    ret
+
+
+
 ;
 ; void _cdecl x86_div64_32(uint64_t dividend, uint32_t divisor, uint64_t* quotientOut, uint32_t* remainderOut);
 ;
@@ -68,6 +101,150 @@ _x86_Video_WriteCharTeletype:
 
     ; restore bx
     pop bx
+
+    ; restore old call frame
+    mov sp, bp
+    pop bp
+    ret
+
+
+;
+; bool _cdecl x86_Disk_Reset(uint8_t drive);
+;
+global _x86_Disk_Reset
+_x86_Disk_Reset:
+
+    ; make new call frame
+    push bp             ; save old call frame
+    mov bp, sp          ; initialize new call frame
+
+    mov ah, 0
+    mov dl, [bp + 4]    ; dl - drive
+    stc
+    int 13h
+
+    mov ax, 1
+    sbb ax, 0           ; 1 on success, 0 on fail   
+
+    ; restore old call frame
+    mov sp, bp
+    pop bp
+    ret
+
+
+;
+; bool _cdecl x86_Disk_Read(uint8_t drive,
+;                           uint16_t cylinder,
+;                           uint16_t sector,
+;                           uint16_t head,
+;                           uint8_t count,
+;                           uint8_t far * dataOut);
+;
+global _x86_Disk_Read
+_x86_Disk_Read:
+
+    ; make new call frame
+    push bp             ; save old call frame
+    mov bp, sp          ; initialize new call frame
+
+    ; save modified regs
+    push bx
+    push es
+
+    ; setup args
+    mov dl, [bp + 4]    ; dl - drive
+
+    mov ch, [bp + 6]    ; ch - cylinder (lower 8 bits)
+    mov cl, [bp + 7]    ; cl - cylinder to bits 6-7
+    shl cl, 6
+    
+    mov dh, [bp + 10]   ; dh - head
+
+    mov al, [bp + 8]    ; cl - sector to bits 0-5
+    and al, 3Fh
+    or cl, al
+
+    mov al, [bp + 12]   ; al - count
+
+    mov bx, [bp + 16]   ; es:bx - far pointer to data out
+    mov es, bx
+    mov bx, [bp + 14]
+
+    ; call int13h
+    mov ah, 02h
+    stc
+    int 13h
+
+    ; set return value
+    mov ax, 1
+    sbb ax, 0           ; 1 on success, 0 on fail   
+
+    ; restore regs
+    pop es
+    pop bx
+
+    ; restore old call frame
+    mov sp, bp
+    pop bp
+    ret
+
+
+;
+; bool _cdecl x86_Disk_GetDriveParams(uint8_t drive,
+;                                     uint8_t* driveTypeOut,
+;                                     uint16_t* cylindersOut,
+;                                     uint16_t* sectorsOut,
+;                                     uint16_t* headsOut);
+;
+global _x86_Disk_GetDriveParams
+_x86_Disk_GetDriveParams:
+
+    ; make new call frame
+    push bp             ; save old call frame
+    mov bp, sp          ; initialize new call frame
+
+    ; save regs
+    push es
+    push bx
+    push si
+    push di
+
+    ; call int13h
+    mov dl, [bp + 4]    ; dl - disk drive
+    mov ah, 08h
+    mov di, 0           ; es:di - 0000:0000
+    mov es, di
+    stc
+    int 13h
+
+    ; return
+    mov ax, 1
+    sbb ax, 0
+
+    ; out params
+    mov si, [bp + 6]    ; drive type from bl
+    mov [si], bl
+
+    mov bl, ch          ; cylinders - lower bits in ch
+    mov bh, cl          ; cylinders - upper bits in cl (6-7)
+    shr bh, 6
+    mov si, [bp + 8]
+    mov [si], bx
+
+    xor ch, ch          ; sectors - lower 5 bits in cl
+    and cl, 3Fh
+    mov si, [bp + 10]
+    mov [si], cx
+
+    mov cl, dh          ; heads - dh
+    mov si, [bp + 12]
+    mov [si], cx
+
+    ; restore regs
+    pop di
+    pop si
+    pop bx
+    pop es
 
     ; restore old call frame
     mov sp, bp
