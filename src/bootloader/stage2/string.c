5fa1400802e1fb485568edaa94091f26eea9b8d1
Ashwin Paudel <62433766+Ashwin-Paudel@users.noreply.github.com>
2021-08-13 19:31:38 -0400

    added more functions

--- src/bootloader/stage2/string.c
+++ src/bootloader/stage2/string.c
@@ -17,6 +17,40 @@ const char* strchr(const char* str, char chr)
     return NULL;
 }
 
+unsigned int strlen(const char* s)
+{
+    unsigned int i = 0;
+    while (s[i] != '\0')
+        ++i;
+    return i;
+}
+
+int strcmp(const char* a, const char* b)
+{
+    while (*a == *b && *a != 0 && *b != 0) {
+        a++;
+        b++;
+    }
+
+    if (a < b) {
+        return -1;
+    }
+    if (a > b) {
+        return 1;
+    }
+    return 0;
+}
+
+void strrev(char s[])
+{
+    int c, i, j;
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
 char* strcpy(char* dst, const char* src)
 {
     char* origDst = dst;
