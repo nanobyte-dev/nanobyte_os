be84ca02258af0656cf66fe4ad8e84e5b51723a7
Trey Tomes <george.tomes@cityelectricsupply.com>
2022-11-30 10:52:55 -0600

    Begin writing a keyboard driver.

--- src/kernel/arch/i686/irq.c
+++ src/kernel/arch/i686/irq.c
@@ -59,7 +59,7 @@ void i686_IRQ_Initialize()
     i686_EnableInterrupts();
 
     // g_Driver->Unmask(0);
-    // g_Driver->Unmask(1);
+    g_Driver->Unmask(1);
 }
 
 void i686_IRQ_RegisterHandler(int irq, IRQHandler handler)
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include "stdio.h"
 #include "memory.h"
@@ -6,6 +7,9 @@
 #include <debug.h>
 #include <boot/bootparams.h>
 
+// For i686_inb/outb.
+#include "arch/i686/io.h"
+
 extern void _init();
 
 void crash_me();
@@ -13,6 +17,27 @@ void crash_me();
 void timer(Registers* regs)
 {
     printf(".");
+}
+
+void keyboard(Registers* regs) {
+    uint8_t scancode = i686_inb(0x60);
+    
+    
+    uint8_t al = i686_inb(0x61);    // keyboard control
+    uint8_t ah = al;
+    al = al | 0x80; // disable bit 7
+    i686_outb(0x61, al); // send it back
+    al = ah;
+    i686_outb(0x61, al); // and send that back
+    
+    bool released = (scancode & 0x80) != 0; // Was the key released?
+    scancode = scancode & 0x7F; // Clip off the key released bit.
+    
+    if (released) {
+        printf("Key released: %d\r\n", scancode);
+    } else {
+        printf("Key pressed: %d\r\n", scancode);
+    }
 }
 
 void start(BootParams* bootParams)
@@ -39,7 +64,8 @@ void start(BootParams* bootParams)
     log_crit("Main", "This is a critical msg!");
     printf("Nanobyte OS v0.1\n");
     printf("This operating system is under construction.\n");
-    //i686_IRQ_RegisterHandler(0, timer);
+    i686_IRQ_RegisterHandler(0, timer);
+    i686_IRQ_RegisterHandler(1, keyboard);
 
     //crash_me();
 
